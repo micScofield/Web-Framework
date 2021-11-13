9aa6355a22ce60a3bc64010bfe76c0fa16fb1431
Sanyam Jain <sanyamjaino98@gmail.com>
2021-11-13 23:21:59 +0530

    Advanced Generic Constraint

--- src/Models/Attributes.ts
+++ src/Models/Attributes.ts
@@ -1,8 +1,14 @@
+/*
+Note: Advanced Generic Constraint
+<K extends keyof T>(key: K): T[K]
+K extends keyof T means that the value of "K" can only be a key of an object "T", and obviously the return value can be set to T[K] instead of a union which we had earlier (eg: string | number | boolean). 
+*/
+
 export class Attributes<T> {
     constructor(private data: T) {}
 
-    get(propName: string): number | string {
-        return this.data[propName];
+    get<K extends keyof T>(key: K): T[K] {
+        return this.data[key];
     }
 
     set(update: T): void {
