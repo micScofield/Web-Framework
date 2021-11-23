cfcbc1163b290cd8ae07206deaae8b96cacd30d4
Sanyam Jain <sanyamjaino98@gmail.com>
2021-11-24 01:14:26 +0530

    Configured UserEdit to map regions

--- src/views/UserEdit.ts
+++ src/views/UserEdit.ts
@@ -1,13 +1,20 @@
-import { User, UserProps } from '../models/User';
-import { View } from './View';
+import { User, UserProps } from "../models/User";
+import { View } from "./View";
 
 export class UserEdit extends View<User, UserProps> {
-  template(): string {
-    return `
+    regionsMap(): { [key: string]: string } {
+        return {
+            userShow: '.user-show',
+            userForm: '.user-form'
+        }
+    }
+
+    template(): string {
+        return `
       <div>
         <div class="user-show"></div>
         <div class="user-form"></div>
       </div>
     `;
-  }
+    }
 }
