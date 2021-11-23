8fba9e2f6c92052731f87b8051a08ab54b7d6610
Sanyam Jain <sanyamjaino98@gmail.com>
2021-11-23 17:19:52 +0530

    Setup index.ts for test

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-import { UserForm } from './views/UserForm';
+// import { UserForm } from './views/UserForm';
+import { UserForm } from './views/test';
+import { UserForm2 } from './views/test2';
 import { User } from './models/User';
 
 const user = User.buildUser({ name: 'NAME', age: 20 });
@@ -7,6 +9,18 @@ const root = document.getElementById('root');
 
 if (root) {
   const userForm = new UserForm(root, user);
+
+  userForm.render();
+} else {
+  throw new Error('Root element not found');
+}
+
+const user2 = User.buildUser({ name: 'Admin'});
+
+const test = document.getElementById('test');
+
+if (test) {
+  const userForm = new UserForm2(test, user2);
 
   userForm.render();
 } else {
