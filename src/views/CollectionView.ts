d6c613eea4e3444c51df37912b4f940530939249
Sanyam Jain <sanyamjaino98@gmail.com>
2021-11-29 01:03:45 +0530

    Render method for Collection View

--- src/views/CollectionView.ts
+++ src/views/CollectionView.ts
@@ -6,6 +6,16 @@ export abstract class CollectionView<T, K> {
   abstract renderItem(model: T, itemParent: Element): void;
 
   render(): void {
-    
+    this.parent.innerHTML = '';
+
+    const templateElement = document.createElement('template');
+
+    for (let model of this.collection.models) {
+      const itemParent = document.createElement('div');
+      this.renderItem(model, itemParent);
+      templateElement.content.append(itemParent);
+    }
+
+    this.parent.append(templateElement.content);
   }
 }
