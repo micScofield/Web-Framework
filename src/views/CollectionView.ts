52e28aa6f675f49d96572ea815753e25790b1583
Sanyam Jain <sanyamjaino98@gmail.com>
2021-11-29 01:03:24 +0530

    Working on a Collectio View

---
+++ src/views/CollectionView.ts
@@ -0,0 +1,11 @@
+import { Collection } from '../models/Collection';
+
+export abstract class CollectionView<T, K> {
+  constructor(public parent: Element, public collection: Collection<T, K>) {}
+
+  abstract renderItem(model: T, itemParent: Element): void;
+
+  render(): void {
+    
+  }
+}
